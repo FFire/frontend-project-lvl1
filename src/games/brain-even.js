5457aa9d15e55f6ff281b09698cf844721655446
overstar <8497715@gmail.com>
2021-07-24 00:35:23 +0500

    User interaction complete

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,10 +1,11 @@
 import action from '../userInteraction.js';
 
 const startGame = () => {
+  const txtDescription = () => 'Answer "yes" if the number is even, otherwise answer "no".';
   const questions = ['qestion 1', 'qestion 2', 'qestion 3'];
-  const answers = [1, 2, 3];
+  const answers = ['1', '2', '3'];
 
-  action(questions, answers);
+  action(txtDescription, questions, answers);
 };
 
 export default startGame;
--- src/userInteraction.js
+++ src/userInteraction.js
@@ -3,29 +3,39 @@ import readlineSync from 'readline-sync';
 const txtWelcome = () => 'Welcome to the Brain Games!';
 const txtWhatIsYourName = () => 'May I have your name? ';
 const txtHello = (userName) => `Hello, ${userName}!`;
-// const txtCongratuation = (userName) => `Congratulations, ${userName}!`;
-// const txtQuestion = (question) => `Question: ${question}`;
-// const txtAnswer = (yourAnswer) => `Your answer: ${yourAnswer}`;
-// const txtCorrect = () => 'Correct!';
-// const txtNonCorrect = (userName, yourAnswer, correctAnswer) => {
-//   const line1 = `${yourAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`;
-//   const line2 = `Let's try again, ${userName}!`;
-//   return `${line1}\n\r${line2}`;
-// };
+const txtCongratuation = (userName) => `Congratulations, ${userName}!`;
+const txtQuestion = (question) => `Question: ${question} `;
+const txtAnswer = (yourAnswer) => `Your answer: ${yourAnswer}`;
+const txtCorrect = () => 'Correct!';
+const txtNonCorrect = (userName, userAnswer, answer) => {
+  const line1 = `${userAnswer} is wrong answer ;(. Correct answer was ${answer}.`;
+  const line2 = `Let's try again, ${userName}!`;
+  return `${line1}\n\r${line2}`;
+};
 
 const getUserName = () => {
   console.log(txtWelcome());
   return readlineSync.question(txtWhatIsYourName());
 };
 
-const action = (questions, answers) => {
-  // for (let i = 0; i < questions.length; i += 1) {
-  //   const question = questions[i];
-  //   const answer = answers[i];
-  // }
+const action = (txtDescription, questions, answers) => {
   const userName = getUserName();
   console.log(txtHello(userName));
+  console.log(txtDescription());
 
-  console.log(`q:${questions} --- a: ${answers}`);
+  for (let i = 0; i < questions.length; i += 1) {
+    const question = questions[i];
+    const answer = answers[i];
+    const userAnswer = readlineSync.question(txtQuestion(question));
+    if (userAnswer === answer) {
+      console.log(txtAnswer(userAnswer));
+      console.log(txtCorrect());
+    } else {
+      console.log(txtNonCorrect(userName, userAnswer, answer));
+      return;
+    }
+  }
+  console.log(txtCongratuation(userName));
 };
+
 export default action;
