90c625737a299d63eac558957ad0383fb1065a98
overstar <8497715@gmail.com>
2021-07-31 22:23:24 +0500

    fix: refactor progression function

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -12,12 +12,8 @@ const progressionSizeMax = 10;
 
 // Generate array of progression
 // generateProgression() : [3, 6, 9, 12, 15, 18 ...and so on]
-const genProgression = () => {
-  const step = getRandomInt(progressionStepMin, progressionStepMax);
-  const start = getRandomInt(progressionStartMin, progressionStartMax);
-  const size = getRandomInt(progressionSizeMin, progressionSizeMax);
+const genProgression = (start = 3, size = 10, step = 3) => {
   const acc = [];
-
   for (let i = 0; i < size; i += 1) {
     acc.push(step * i + start);
   }
@@ -25,7 +21,10 @@ const genProgression = () => {
 };
 
 const genDataItem = () => {
-  const progression = genProgression();
+  const step = getRandomInt(progressionStepMin, progressionStepMax);
+  const start = getRandomInt(progressionStartMin, progressionStartMax);
+  const size = getRandomInt(progressionSizeMin, progressionSizeMax);
+  const progression = genProgression(start, size, step);
   const rndIndex = getRandomInt(0, progression.length - 1);
   const answer = progression[rndIndex].toString();
   progression[rndIndex] = '..';
